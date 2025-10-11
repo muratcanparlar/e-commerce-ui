8c529ce6ea2487d955a5e5af0655f3ace290d897
Murat Can PARLAR <canparlar@hotmail.com>
2025-10-11 19:38:49 +0300

    Developed ProductCart and ProductList component

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -2,6 +2,7 @@ import { ProductsType } from "@/types";
 import React from "react";
 import Categories from "./Categories";
 import ProductCard from "./ProductCard";
+import Link from "next/link";
 
 // TEMPORARY
 const products: ProductsType = [
@@ -115,7 +116,7 @@ const products: ProductsType = [
   },
 ];
 
-const ProductList = () => {
+const ProductList = ({ category }: { category: string }) => {
   return (
     <div className="w-full">
       <Categories />
@@ -124,6 +125,12 @@ const ProductList = () => {
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+      <Link
+        href={category ? `/products/?category=${category}` : "/products"}
+        className="flex justify-end mt-4 underline text-sm text-gray-500"
+      >
+        View all products
+      </Link>
     </div>
   );
 };
