0be170f8cc1199650ba0cffc43bc090641ce23b7
Murat Can PARLAR <canparlar@hotmail.com>
2025-10-11 03:28:21 +0300

    Home page design is completed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import Image from "next/image";
+
 const Homepage = () => {
-  return <div className="">Homepage</div>;
+  return (
+    <div className="">
+      <div className="relative aspect-[3/1] mb-12">
+        <Image src="/featured.png" alt="Featured Product" fill />
+      </div>
+    </div>
+  );
 };
 
 export default Homepage;
